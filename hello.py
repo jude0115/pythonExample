9dbf73ed8da44f519a481a0a78f542f4a6f04447
jude0115 <jude.sishih@gmail.com>
2019-01-11 00:11:10 +0800

    Add more examples

--- hello.py
+++ hello.py
@@ -1,1 +1,29 @@
-print("Hello Python")
+# Simple print
+print('Hello Python')
+
+# input
+name = input('Please write your name')
+print 'Hello, ' + name
+
+# for loop
+print 'for loop' 
+for i in range(0,10):
+	print i
+
+#while loop
+print 'while loop'
+x = 0
+while x < 10:
+	x+=1
+	print x
+
+#function
+def add(var1, var2):
+	return var1 + var2
+
+print 'function'
+var1 = input('Please type first number')
+var2 = input('Please type second number')
+result = add(var1, var2)
+print 'result = '  
+print result
